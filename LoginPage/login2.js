36ece7b124b4b281ae3a3507cc09d507a004ce7e
AjayKumar27-03 <ajaykumar.1da19mca53@gmail.com>
2020-12-04 00:09:24 +0530

    Hello

---
+++ LoginPage/login2.js
@@ -0,0 +1,17 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="manu"&& password=="manu")
+{
+    alert("login succesfully");
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
